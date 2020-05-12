de8b9b35aa1ea1c305ec62e50f5794e568eb035f
Pablo Lacerda de Miranda <placerdademiranda@netflix.com>
2020-05-11 17:08:10 -0700

    connects API and component through useApi hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,42 @@
 import React from 'react';
 import './App.css';
-import {start} from "./api";
+import { start, getStatus } from './api';
+import { stat } from 'fs';
+
+function useApi() {
+  const [status, setStatus] = React.useState({
+    status: 'NOTHING',
+    estimatedTime: 0,
+  });
+
+  getStatus().then((response) => {
+    setStatus(status);
+  });
+
+  const getData = () => {
+    start().then((resp) => {
+      const { ok, status, estimatedTime } = resp;
+      if (ok) {
+        setStatus({
+          status,
+          estimatedTime,
+        });
+      }
+    });
+  };
+
+  return {
+    ...status,
+    getData,
+  };
+}
 
 function App() {
+  const { status, getData } = useApi();
   return (
     <div className="App">
-      <button onClick={start}>start</button>
+      <button onClick={getData}>start</button>
+      <div>{status}</div>
     </div>
   );
 }
