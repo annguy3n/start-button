147f117e038f33d692fa6260bd4a4dfe71cadb7a
Pablo Lacerda de Miranda <placerdademiranda@netflix.com>
2020-05-11 17:40:51 -0700

    moves hook code to its own file

--- src/App.tsx
+++ src/App.tsx
@@ -1,63 +1,7 @@
 import React from 'react';
 import './App.css';
-import { start, getStatus, Status } from './api';
+import useApi from './useApi';
 import { stat } from 'fs';
-
-type ApiStatusResponse = {
-  ok: boolean;
-  status: Status;
-  estimatedTime: number;
-};
-
-function useApi() {
-  const [status, setStatus] = React.useState<Partial<ApiStatusResponse>>({
-    status: null,
-    estimatedTime: 0,
-  });
-
-  // Define a ref object to avoid a timer to be created
-  // on every component update
-  const timer = React.useRef<number>(0);
-
-  const checkStatus = React.useCallback(() => {
-    getStatus().then((response: Partial<ApiStatusResponse>) => {
-      if (status.status !== response.status) {
-        setStatus(response);
-        if (response.status === 'Done') {
-          clearInterval(timer.current);
-        }
-      }
-    });
-  }, [status.status]);
-
-  const runStatusPoll = React.useCallback(() => {
-    if (timer.current) {
-      clearInterval(timer.current);
-    }
-    // Need to use the explicit window object reference here
-    // to satisfy the TypeScript compiler and correct fix
-    // the evaluated type returned by setInterval
-    timer.current = window.setInterval(checkStatus, 5000);
-  }, [checkStatus]);
-
-  const getData = React.useCallback(() => {
-    start().then((resp) => {
-      const { ok, status, estimatedTime } = resp as ApiStatusResponse;
-      if (ok) {
-        setStatus({
-          status,
-          estimatedTime,
-        });
-        runStatusPoll();
-      }
-    });
-  }, [runStatusPoll]);
-
-  return {
-    ...status,
-    getData,
-  };
-}
 
 function App() {
   const { status, getData } = useApi();
